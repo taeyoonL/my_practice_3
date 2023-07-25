da3b517f5625d05c8bf6a297283cd72ef3a1add3
Taeyoon Lee <132141316+taeyoonL@users.noreply.github.com>
2023-07-26 00:11:14 +0900

    Update ViewController.swift

--- my_practice_3/ViewController.swift
+++ my_practice_3/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    @IBOutlet var picker_view: UIPickerView!
+    @IBOutlet var label: UILabel!
+    @IBOutlet var image_view: UIImageView!
+    let data_count = 7
+    let row = 2
+    var image_text = ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg"]
+    var image_array = [UIImage?]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for i in 0..<data_count {
+            let image = UIImage(named: image_text[i])
+            image_array.append(image)
+        }
+        label.text = image_text[0]
+        image_view.image = image_array[0]
     }
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return row
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return data_count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let imageview = UIImageView(image: image_array[row])
+        imageview.frame = CGRect(x: 0, y: 0, width: 100, height: 150)
+        return imageview
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 0 {
+            label.text = image_text[row]
+        } else {
+            image_view.image = image_array[row]
+        }
+    }
 
 }
 
